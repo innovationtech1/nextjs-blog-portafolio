445972c406ef62ce6bf29dc89aa08de776ccf53c
Hector De Hoyos <hectordehoyos053@gmail.com>
2023-02-12 22:37:31 -0600

    Se actualizaron archivos

--- pages/_document.js
+++ pages/_document.js
@@ -9,13 +9,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <title> InnovationTECH </title>{" "}
-
+        <title> InnovationTECH </title>
         <Head>
-          <link
-            rel="shortcut icon"            
-            href="/favicon/favicon.ico"
-          />
+          <link rel="shortcut icon" href="/favicon/favicon.ico" />
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css"
@@ -27,32 +23,30 @@ class MyDocument extends Document {
             href="https://use.fontawesome.com/releases/v5.15.2/css/all.css"
             integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu"
             crossorigin="anonymous"
-          ></link>{" "}
+          ></link>
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
             integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
             crossorigin="anonymous"
-          ></link>{" "}
+          ></link>
           <script
             src="https://code.jquery.com/jquery-3.6.0.min.js"
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
             crossorigin="anonymous"
           ></script>
         </Head>
-     
-<script
-  src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-  integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-  crossorigin="anonymous"
-></script>
-<script
-  src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-  integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-  crossorigin="anonymous"
-></script>
-
-        <body className="style">          
+        <script
+          src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+          integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+          integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+          crossorigin="anonymous"
+        ></script>
+        <body className="style">
           <Main />
           <NextScript />
         </body>
